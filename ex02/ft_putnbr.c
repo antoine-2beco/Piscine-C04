a80f4039531a7b6bbafc1a2315e12fa54a893f7a
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2023-09-14 11:18:37 +0200

    ex00 to 02

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 11:05:07 by ade-beco          #+#    #+#             */
+/*   Updated: 2023/09/14 11:05:08 by ade-beco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(int a)
+{
+	write(1 , &a, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = - nb;
+		ft_putnbr(nb);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+/*int main(void)
+{
+	ft_putnbr(-2147483648);
+	return (0);
+}*/
